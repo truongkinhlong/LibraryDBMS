cfe01f13ee9cc68342c60d8db73e26c73ba8a61d
truongkinhlong <116505384+truongkinhlong@users.noreply.github.com>
2023-04-20 17:14:43 -0500

    Update createDB.sql

--- createDB.sql
+++ createDB.sql
@@ -6,14 +6,13 @@ CREATE TABLE Branch (
   address VARCHAR(255) NOT NULL,
   openHour TIME NOT NULL,
   closeHour TIME NOT NULL,
-  openD ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL,
-  adminID INT NOT NULL UNIQUE,
+  openDay SET('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL,
   PRIMARY KEY (branchID)
 );
 
 CREATE TABLE Admin (
   adminID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
-  title VARCHAR(10) NOT NULL,
+  title VARCHAR(50) NOT NULL,
   lName VARCHAR(50) NOT NULL,
   fName VARCHAR(50) NOT NULL,
   age INT NOT NULL,
@@ -26,7 +25,7 @@ CREATE TABLE Admin (
 
 CREATE TABLE Staff (
   staffID INT NOT NULL UNIQUE KEY AUTO_INCREMENT,
-  title VARCHAR(10) NOT NULL,
+  title VARCHAR(50) NOT NULL,
   lName VARCHAR(50) NOT NULL,
   fName VARCHAR(50) NOT NULL,
   age INT NOT NULL,
@@ -45,7 +44,7 @@ CREATE TABLE Member (
   sex ENUM('M', 'F') NOT NULL,
   DOB DATE NOT NULL,
   email VARCHAR(255) NOT NULL,
-  status BOOLEAN NOT NULL,
+  status ENUM('Active', 'Expired') NOT NULL,
   hold BOOLEAN NOT NULL,
   PRIMARY KEY (memberID)
 );
@@ -56,7 +55,7 @@ CREATE TABLE Book (
   title VARCHAR(255) NOT NULL,
   branchID INT NOT NULL,
   status ENUM('Available', 'On Loan', 'Lost') NOT NULL,
-  publishBy VARCHAR(255) NOT NULL,
+  publishedBy VARCHAR(255) NOT NULL,
   language VARCHAR(50) NOT NULL,
   shelf VARCHAR(50) NOT NULL,
   PRIMARY KEY (isbn),
@@ -78,10 +77,10 @@ CREATE TABLE Request (
 );
 
 CREATE TABLE Register (
-  username VARCHAR(50) NOT NULL,
+  username VARCHAR(255) NOT NULL,
   password VARCHAR(50) NOT NULL,
-  staffID INT NOT NULL,
-  memberID INT NOT NULL,
+  staffID INT ,
+  memberID INT,
   PRIMARY KEY (username),
   FOREIGN KEY (staffID) REFERENCES Staff(staffID),
   FOREIGN KEY (memberID) REFERENCES Member(memberID)
@@ -92,11 +91,17 @@ CREATE TABLE `Order` (
   requestID INT NOT NULL,
   borrowDate DATE NOT NULL,
   returnDate DATE NOT NULL,
-  status ENUM('Pending', 'Active', 'Returned') NOT NULL,
+  status ENUM('Pending', 'Active', 'Returned', 'Overdue') NOT NULL,
   dueDate DATE NOT NULL,
   accuredFine DECIMAL(10,2) NOT NULL,
   PRIMARY KEY (orderID),
   FOREIGN KEY (requestID) REFERENCES Request(requestID)
+);
+
+CREATE TABLE Author (
+  authorID INT PRIMARY KEY AUTO_INCREMENT,
+  lName VARCHAR(255) NOT NULL,
+  fName VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE WrittenBy (
@@ -107,8 +112,3 @@ CREATE TABLE WrittenBy (
   FOREIGN KEY (authorID) REFERENCES Author(authorID)
 );
 
-CREATE TABLE Author (
-  authorID INT PRIMARY KEY AUTO_INCREMENT,
-  lName VARCHAR(255) NOT NULL,
-  fName VARCHAR(255) NOT NULL
-);
